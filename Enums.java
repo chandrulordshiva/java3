25a657267d9a85b42c1af9dc808d9e166eec516d
chandrulordshiva <chandrulordshiva@zohomail.in>
2023-03-13 08:17:46 +0530

    enums class

---
+++ Enums.java
@@ -0,0 +1,14 @@
+enum Error{
+    LORDSHIVA,
+    CHANDRU,
+    joker,
+    hello_world,
+
+}
+class Enums{
+    public static void main(String args[])
+    {
+        Error a= Error.LORDSHIVA;
+        System.out.println(a);
+    }
+}
